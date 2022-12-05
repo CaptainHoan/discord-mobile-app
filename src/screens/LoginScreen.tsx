33dc49e8ad98ddbc558708e1a7345226798ce65f
captain <hoancaptain123456789@gmail.com>
2022-12-05 11:03:05 +0700

    add Login component

---
+++ src/screens/LoginScreen.tsx
@@ -0,0 +1,75 @@
+import { View, Text, SafeAreaView, TextInput, TouchableOpacity } from 'react-native'
+import React, {useState} from 'react'
+
+
+const LoginScreen = () => {
+  return (
+    <SafeAreaView className='bg-gray-700 flex-1'>
+        <View className='mt-10 items-center justify-center mb-8'>
+            <Text className='text-2xl font-bold text-white mb-3'>Welcome back</Text>
+            <Text className='text-md font-bold text-gray-400'>We're so excited to see you again</Text>
+        </View>
+        <View className='mx-5'>
+            <Text className='text-md text-gray-300 font-extrabold mb-2'>ACCOUNT INFORMATION</Text>
+            <LoginForm />
+        </View>
+      
+    </SafeAreaView>
+  )
+}
+
+const LoginForm = () => {
+
+    const [email, setEmail] = useState<string>('')
+    const [password, setPassword] = useState<string>('')
+
+    return (
+        <View>
+            <View style={{
+                backgroundColor: 'black',
+                padding: 15,
+                borderRadius: 8,
+                marginBottom: 10
+            }}>
+                <TextInput 
+                    placeholder='Email'
+                    placeholderTextColor={'gray'}
+                    value={email}
+                    onChangeText={value => setEmail(value)}
+                    style={{
+                        color: 'white',
+                        fontSize: 18
+                    }}
+                />
+            </View>
+
+            <View style={{
+                backgroundColor: 'black',
+                padding: 15,
+                borderRadius: 8
+            }}>
+                <TextInput 
+                    placeholder='Password'
+                    placeholderTextColor={'gray'}
+                    value={password}
+                    onChangeText={value => setPassword(value)}
+                    style={{
+                        color: 'white',
+                        fontSize: 18
+                    }}
+                />
+            </View>
+            <TouchableOpacity className='mt-4 mb-6'>
+                <Text className='text-sm text-blue-300'>forgot your password?</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity className='bg-blue-400 p-3 rounded-md '>
+                <Text className='text-white font-bold text-lg text-center '>Log In</Text>
+            </TouchableOpacity>
+            
+            
+        </View>
+    )
+}
+
+export default LoginScreen
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,9 @@
 module.exports = {
   content: [
     "./App.{js,jsx,ts,tsx}", 
-    "./src/Screens/*.{js,jsx,ts,tsx}",
-    "./src/Components/*.{js,jsx,ts,tsx}"
+    "./src/screens/*.{js,jsx,ts,tsx}",
+    "./src/components/*.{js,jsx,ts,tsx}",
+    "./src/navigation/*.{js,jsx,ts,tsx}"
   ],
   theme: {
     extend: {},
